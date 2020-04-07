5d89ce7f725af652d34260b59bf018c3a25f04b9
BuildTools <unconfigured@null.spigotmc.org>
2020-04-07 16:51:53 +0200

    update

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+$(function() {
+  $(".selectable").selectable({
+    selected: function() {
+      var selectedItemList = $("#selected-item-list").empty();
+      $(".selectable img").each(function(index) {
+        if ($(this).hasClass("ui-selected")) {
+          selectedItemList.append((index + 1) + ", ");
+        }
+      });
+    }
+  });
+});
+
+});
